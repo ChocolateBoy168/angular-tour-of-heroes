2f63e81b0e0cd72be6912b62fb8014b596a93ff9
Andrew <chokali168@gmail.com>
2018-12-10 00:51:10 +0800

    FormsModule

--- doc/note.md
+++ doc/note.md
@@ -10,10 +10,37 @@
 * ng test
 * ng e2e
 * ng generate component heroes
+* ng generate --help
+* ng generate component --help
+* ng build --help
+* ng build default(app)
+* ng build {projectName}
 
 #過往指令
 * npm list --depth=0
 * npm view angular
 * npm view angular version
 * npm run build 等同 => ng build
-* npm install => windows下從github抓下來, 下此指令,產生node_modules
+* npm install or npm i=> windows下從github抓下來, 下此指令,產生node_modules
+* npm info {套件名稱} version
+* npm uninstall {套件名稱} ==> 移除套件
+
+#Other
+(funciton)
+(input) 
+(click) => bind-click
+(change)
+routerLink = 'abc'
+[routerLink] = "'abc'"
+[(ngModel)]
+[] = 運算式
+*ngFor => * (exist or not exist)
+ngIf
+
+#框架
+workspace 
+-> project(application) , web app or mobile app 
+-> module 
+-> component
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/* AppModule decorators*/
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core'; // <-- critical metadata
+import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
 
+/*The most important @NgModule decorator annotates the top-level AppModule class.*/
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
