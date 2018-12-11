47decda3dfb738b8a5ad68ffcc3c0fd0e449b249
chocolate <chokali168@gmail.com>
2018-12-12 02:37:08 +0800

    HeroService @Injectable ngOnInit Rxjs Observable of

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from "../mock-heroes";
+//import {HEROES} from "../mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -14,20 +15,29 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() {
+  //Add a private heroService parameter of type HeroService to the constructor.
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   selectedHero: Hero;
+
   //selectedHero  = HEROES[0]
 
   onSelect(hero: Hero): void {
     console.log(hero);
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    //this.heroes = this.heroService.getHeroes(); // ==>  service / getHeroes():Hero[]{ return HEROES;}
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes); // ==>  service / getHeroes():Observable<Hero[]>{return of(HEROES);}
   }
 
 }
