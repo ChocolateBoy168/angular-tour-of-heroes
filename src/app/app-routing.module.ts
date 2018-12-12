cbaa751ce9e881f8350bcbcfd102ccc11bcff87a
Andrew <chokali168@gmail.com>
2018-12-12 17:19:59 +0800

    Add a navigation link (routerLink)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,26 @@ import {NgModule} from '@angular/core';
 
 // import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
+
+const rotes: Routes = [{
+  path: 'heroes', component: HeroesComponent
+}];
 
 @NgModule({
   // declarations: [],
   imports: [
     // CommonModule
+
+    /**
+     * You first must initialize the router and start it listening for browser location changes.
+     */
+    RouterModule.forRoot(rotes)
   ],
+  /**
+   * Add an @NgModule.exports array with RouterModule in it.
+   * Exporting RouterModule makes router directives available for use in the AppModule components that will need them.
+   */
   exports: [
     RouterModule
   ]
