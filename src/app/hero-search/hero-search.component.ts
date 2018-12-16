4c798048e1766f7d12ba095dbbe724bbc289e592
chocolate <chokali168@gmail.com>
2018-12-17 01:16:43 +0800

    The searchTerms RxJS subject

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, Subject} from "rxjs";
+import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
+import {debounceTime, distinctUntilChanged, switchAll, switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-hero-search',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSearchComponent implements OnInit {
 
-  constructor() { }
+  private heroes$: Observable<Hero[]>;//Notice the declaration of heroes$ as an Observable
+  private searchTerms = new Subject<string>();//The searchTerms property is declared as an RxJS Subject.
+
+  constructor(private heroService: HeroService) {
+  }
+
+  // Push a search term into the observable stream.
+  /**
+   * 1. A Subject is both a source of observable values and an Observable itself.
+   *    You can subscribe to a Subject as you would any Observable.
+   * 2. You can also push values into that Observable by calling its next(value) method as the search() method does.
+   */
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit() {
+
+    /**
+     * 1. Every time the user types in the textbox, the binding calls search() with the textbox value, a "search term".
+     *   The searchTerms becomes an Observable emitting a steady stream of search terms.
+     */
+    this.heroes$ = this.searchTerms.pipe(
+      //wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      //ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      //switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
   }
 
 }
